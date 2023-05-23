e7ae8f6fe01cac9516ca4e5de29ee8280213e7e8
HyeJin Ryoo <56506753+hjryoo@users.noreply.github.com>
2023-05-23 22:15:13 +0900

    start new project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './app';
+
+import './shared/crash';
+import './shared/service-worker';
+import './shared/vendor';
+import './styles/styles.scss';
+
+render(<App />, document.getElementById('app'));
